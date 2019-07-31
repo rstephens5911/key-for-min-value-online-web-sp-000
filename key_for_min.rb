04463153d7caad9a8b9322298c7050a4623602b2
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-31 16:24:44 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,10 @@
 # name = key
 # value = value
 def key_for_min_value(name_hash)
-  min_value = 100
+  min_value = nil
   min_key = nil
   name_hash.collect do |name, value|
-    if min_value > value.to_i
+    if min_value == nil || min_value > value
       min_value = value
       min_key = name
     end
