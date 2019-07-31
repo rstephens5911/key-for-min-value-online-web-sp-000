eea672b67847bbaaf3fcc62862bbebcc9c48f969
Ronald Stephens <rstephens@bluemorpho.cc>
2019-07-31 16:24:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
   name_hash.collect do |name, value|
-    if min_value > value.to_i
+    if min_value == nil || min_value > value
       min_value = value
       min_key = name
     end
